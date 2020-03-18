9f80170d41a4efc5009f932eb0153c37988faeb1
jlee335 <21jlee335@gmail.com>
2020-03-19 03:41:27 +0900

    c

--- game.js
+++ game.js
@@ -1,48 +1,73 @@
+/*
+ * Author: Jerome Renaux
+ * E-mail: jerome.renaux@gmail.com
+ */
 
-
-
-//Many parts of the code follows tutorial from https://github.com/Jerenaux/basic-mmo-phaser/blob/master/js/game.js
-
-
-
-var game = new Phaser.Game(16*32, 600, Phaser.AUTO, document.getElementById('game'));
-game.state.add('Game',Game);
-game.state.start('Game');
 var Game = {};
 
-/*
-Client orders will be sent to server. Game.function will change server state.
-*/
+var DebugText;
+
+var Terrain_Layer;
+var Building_Layer;
+var UI_Layer;
+
+
 
 Game.init = function(){
     game.stage.disableVisibilityChange = true;
 };
 
 Game.preload = function() {
-	/*
-	MAP tilesheet.
-	--> map data will be saved in json format!!!
-	--> spritesheet brings out texture?
-
-	*/
     game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
-    game.load.image('sprite','assets/sprites/sprite.png'); // this will be the sprite of the players
+    game.load.image('sprite','assets/sprites/sprite.png');
 };
 
 Game.create = function(){
-	Game.playerList = {}; // 플레이어 리스트 
-	var map = game.add.tilemap('map'); // 맵 추가 
+    Game.playerMap = {};
+    var testKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+    testKey.onDown.add(Client.sendTest, this);
 
+    var map = game.add.tilemap('map');
     map.addTilesetImage('tilesheet', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
     var layer;
     for(var i = 0; i < map.layers.length; i++) {
         layer = map.createLayer(i);
-    }//레이어?
-    
-    layer.inputEnabled = true; // Allows clicking on the map
-}
+    }
+
+    layer.inputEnabled = true; // Allows clicking on the map ; it's enough to do it on the last layer
+    layer.events.onInputUp.add(Game.getCoordinates, this);
+
+    DebugText = game.add.text(game.world.centerX, game.world.centerY, "hii", {
+        font: "65px Arial",
+        fill: "#ff0044",
+        align: "center"
+    });
+
+    DebugText.anchor.setTo(0.5, 0.5);
+
+    Client.askNewPlayer();
+};
+
+Game.getCoordinates = function(layer,pointer){
+    Client.sendClick(pointer.worldX,pointer.worldY);
+};
 
 Game.addNewPlayer = function(id,x,y){
     Game.playerMap[id] = game.add.sprite(x,y,'sprite');
+    DebugText.setText('New Player! ' + id);
+};
+
+Game.movePlayer = function(id,x,y){
+    var player = Game.playerMap[id];
+    var distance = Phaser.Math.distance(player.x,player.y,x,y);
+    var tween = game.add.tween(player);
+    var duration = distance*10;
+    tween.to({x:x,y:y}, duration);
+    tween.start();
+};
+
+Game.removePlayer = function(id){
+    Game.playerMap[id].destroy();
+    delete Game.playerMap[id];
 };
--- server.js
+++ server.js
@@ -1,105 +1,51 @@
-
-/*
-
-TODO::
-
-*/
-
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
-//Socket.io will be used!!!
-
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
 app.use('/assets',express.static(__dirname + '/assets'));
 
-
-//GET request for initial website, we send client file of the game
 app.get('/',function(req,res){
-    res.sendFile(__dirname+'/game.html');
+    res.sendFile(__dirname+'/index.html');
 });
 
+server.lastPlayderID = 0;
 
-server.listen(8081,function(){ // Listens to port 8081
-    console.log('Listening on '+ server.address().port);
+server.listen(process.env.PORT || 8081,function(){
+    console.log('Listening on '+server.address().port);
 });
-
-server.lastPlayerID = 0;
-speed = 0.01;
 
 io.on('connection',function(socket){
-	// 새로운 플레이어가 들어올 때 socket connection 별로...
-	/*
-	Login 기능 및 이름 정하기, 외모 정하기, etc 는 여기서 구현....
-	*/
 
+    socket.on('newplayer',function(){
+        socket.player = {
+            id: server.lastPlayderID++,
+            x: randomInt(100,400),
+            y: randomInt(100,400)
+        };
+        socket.emit('allplayers',getAllPlayers());
+        socket.broadcast.emit('newplayer',socket.player);
 
-	//	완전 새로운 플레이어로 접속할 떄
-	socket.on('newPlayer',function(){
-		//TODO: 위에서 정한 이름, 외모, etc 특성들을 가지고 있는 Player Object 를 구현할 것!
+        socket.on('click',function(data){
+            console.log('click to '+data.x+', '+data.y);
+            socket.player.x = data.x;
+            socket.player.y = data.y;
+            io.emit('move',socket.player);
+        });
 
-		// 1. Recieve Player parameters (TODO)
+        socket.on('disconnect',function(){
+            io.emit('remove',socket.player.id);
+        });
+    });
 
-		// 2. Generate Player Object
-		socket.player = {
-			id: 	server.lastPlayerID++,
-			name:   "이현재",
-			x: 		randomInt(100,100),
-			y: 		randomInt(100,100)  
-		};
-
-		// 3. Update Every Client of new player
-		socket.emit('allPlayers',getAllPlayers()); 			// Update Self
-		socket.broadcast.emit('newPlayer',socket.player); 	// Update Everyone Else
-	});
-
-	// 	기존 플레이어가 게임에 다시 들어올 떄, Player Object 를 맏아 한다!
-	socket.on('newLogin',function(player){
-		socket.emit('allPlayers',getAllPlayers());
-		socket.broadcast.emit('newPlayer',player);
-	})
-
-
-	//	플레이어가 키를 누를 떄
-	//  Q: key Press 를 이렇게 보내는게 최선일까? Map Update 주기와 Server-Client Sync 를 고민해야 한다ㅠㅠ 
-	socket.on('keyPress',function(key)){
-		if(key == 'w')
-		{
-			socket.player.y += speed;
-		}
-		else if(key == 'a')
-		{
-			socket.player.x -= speed;
-		}
-		else if(key == 's')
-		{
-			socket.player.y -= speed;
-		}
-		else if(key == 'd')
-		{
-			socket.player.x += speed;
-		}
-		else
-		{
-
-		}
-	}
-
-
+    socket.on('test',function(){
+        console.log('test received');
+    });
 });
 
-
-
-
-/*
-Necessary Auxiliary Functions
-*/
-
 function getAllPlayers(){
-	// getAllPlayers returns a List of all Player Objects!!!
     var players = [];
     Object.keys(io.sockets.connected).forEach(function(socketID){
         var player = io.sockets.connected[socketID].player;
@@ -108,7 +54,6 @@ function getAllPlayers(){
     return players;
 }
 
-//Random number generation function
 function randomInt (low, high) {
     return Math.floor(Math.random() * (high - low) + low);
 }
